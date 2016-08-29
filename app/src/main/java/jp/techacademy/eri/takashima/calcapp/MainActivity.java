a4dc4868b4d0cd43ce412eabf9ae6067a6450cf7
Eri Takashima <efuruk@gmail.com>
2016-08-28 19:49:35 -0500

    空白の時のエラー表示を追加。

--- app/src/main/java/jp/techacademy/eri/takashima/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/eri/takashima/calcapp/MainActivity.java
@@ -10,10 +10,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.view.ViewGroup;
 
 
 public class MainActivity extends AppCompatActivity {
-
     TextView mTextView;
     EditText mEditText;
 
@@ -30,73 +31,98 @@ public class MainActivity extends AppCompatActivity {
                 editText1.setInputType(InputType.TYPE_CLASS_NUMBER);
                 EditText editText2 = (EditText) findViewById(R.id.editText2);
                 editText2.setInputType(InputType.TYPE_CLASS_NUMBER);
-
                 String VALUE1 = editText1.getText().toString();
                 String VALUE2 = editText2.getText().toString();
-                Double A = Double.parseDouble(VALUE1);
-                Double B = Double.parseDouble(VALUE2);
-                Double result = A + B;
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("VALUEC", result);
-                startActivity(intent);
+
+                if (editText1.getText().length()==0) {
+                    editText1.setError("数値をお願いします");
+                } else if (editText2.getText().length()==0) {
+                    editText2.setError("数値をお願いします");
+                } else {
+                    Double A = Double.parseDouble(VALUE1);
+                    Double B = Double.parseDouble(VALUE2);
+                    Double result = A + B;
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("VALUEC", result);
+                    startActivity(intent);
+                }
             }
         });
         Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+        public void onClick(View v) {
                 EditText editText1 = (EditText) findViewById(R.id.editText1);
                 editText1.setInputType(InputType.TYPE_CLASS_NUMBER);
                 EditText editText2 = (EditText) findViewById(R.id.editText2);
                 editText2.setInputType(InputType.TYPE_CLASS_NUMBER);
-
                 String VALUE1 = editText1.getText().toString();
                 String VALUE2 = editText2.getText().toString();
-                Double A = Double.parseDouble(VALUE1);
-                Double B = Double.parseDouble(VALUE2);
-                Double result = A - B;
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("VALUEC", result);
-                startActivity(intent);
+
+                if (editText1.getText().length()==0) {
+                    editText1.setError("数値をお願いします");
+                } else if(editText2.getText().length() == 0) {
+                    editText2.setError("数値をお願いします");
+                } else {
+                    Double A = Double.parseDouble(VALUE1);
+                    Double B = Double.parseDouble(VALUE2);
+                    Double result = A-B;
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("VALUEC", result);
+                    startActivity(intent);
+                }
             }
         });
         Button button3 = (Button) findViewById(R.id.button3);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick (View v) {
                 EditText editText1 = (EditText) findViewById(R.id.editText1);
                 editText1.setInputType(InputType.TYPE_CLASS_NUMBER);
                 EditText editText2 = (EditText) findViewById(R.id.editText2);
                 editText2.setInputType(InputType.TYPE_CLASS_NUMBER);
-
                 String VALUE1 = editText1.getText().toString();
                 String VALUE2 = editText2.getText().toString();
-                Double A = Double.parseDouble(VALUE1);
-                Double B = Double.parseDouble(VALUE2);
-                Double result = A*B;
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("VALUEC", result);
-                startActivity(intent);
+
+                if(editText1.getText().length()==0) {
+                    editText1.setError("数値をお願いします");
+                } else if(editText2.getText().length()==0) {
+                    editText2.setError("数値をお願いします");
+                } else {
+                    Double A = Double.parseDouble(VALUE1);
+                    Double B = Double.parseDouble(VALUE2);
+                    Double result = A * B;
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("VALUEC", result);
+                    startActivity(intent);
+                }
             }
         });
         Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick (View v){
                 EditText editText1 = (EditText) findViewById(R.id.editText1);
                 editText1.setInputType(InputType.TYPE_CLASS_NUMBER);
                 EditText editText2 = (EditText) findViewById(R.id.editText2);
                 editText2.setInputType(InputType.TYPE_CLASS_NUMBER);
-
                 String VALUE1 = editText1.getText().toString();
                 String VALUE2 = editText2.getText().toString();
-                Double A = Double.parseDouble(VALUE1);
-                Double B = Double.parseDouble(VALUE2);
-                Double result = A/B;
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("VALUEC", result);
-                startActivity(intent);
+
+                if(editText1.getText().length()==0) {
+                    editText1.setError("数値をお願いします");
+                } else if(editText2.getText().length()==0) {
+                    editText2.setError("数値をお願いします");
+                } else {
+                    Double A = Double.parseDouble(VALUE1);
+                    Double B = Double.parseDouble(VALUE2);
+                    Double result = A/B;
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("VALUEC", result);
+                    startActivity(intent);
+                }
             }
         });
     }
 }
+
